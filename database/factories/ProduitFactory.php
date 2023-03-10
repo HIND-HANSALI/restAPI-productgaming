08cce2cd33fa524ee24218669cbe4796f81fd02b
HIND-HANSALI <hindhsl111@gmail.com>
2023-03-10 16:08:26 +0100

    other files

---
+++ database/factories/ProduitFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Produit>
+ */
+class ProduitFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "titreProduit" => fake()->name(),
+            "description" => fake()->name(),
+            "contenu" => fake()->name(),
+            "prix"=> fake()->numberBetween(1, 3),
+            // "categorie_id" => fake()->numberBetween(1, 3),
+            
+            "user_id" => fake()->numberBetween(1, 3),
+        ];
+    }
+}
