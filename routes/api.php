81bbe1720839679fe535759a2f9fe542203998d1
HIND-HANSALI <hindhsl111@gmail.com>
2023-03-10 16:03:58 +0100

    add route produit et categorie

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ProduitController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::group(['controller' => CategorieController::class], function () {
+            Route::get('/categories', 'index');
+            Route::post('/categories', 'store');
+            Route::get('/categories/{category}', 'show');
+            Route::put('/categories/{category}', 'update');
+            Route::delete('/categories/{category}', 'destroy');
+});
+Route::group(['controller' => ProduitController::class], function () {
+    Route::get('/produits', 'index');
+    Route::get('/produits/{id}','show');
+    Route::post('/produits','store');
+    Route::put('/produits/{id}', 'update');
+    Route::delete('/produits/{id}', 'destroy');
+});
