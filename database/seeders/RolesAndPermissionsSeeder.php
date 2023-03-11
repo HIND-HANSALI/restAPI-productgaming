ee9f2d9d511c2bfd6b59769272ca336fdcbb3af6
HIND-HANSALI <hindhsl111@gmail.com>
2023-03-11 23:27:58 +0100

    create roles and assign created permissions

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Spatie\Permission\Models\Role;
+
 use Spatie\Permission\Models\Permission;
 use Illuminate\Database\Seeder;
 
